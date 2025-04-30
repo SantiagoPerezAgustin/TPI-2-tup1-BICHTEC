7bd8a0ca0a9f1a508c4f53d11435c2f1353c8fb0
SantiagoPerezAgustin <santiagus153@gmail.com>
2025-04-30 12:08:05 -0300

    Publico navBar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
-
+import React from "react";
+import Header from "./components/Header/Header.jsx";
+import Home from "./pages/Home/Home.jsx";
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <Header />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
