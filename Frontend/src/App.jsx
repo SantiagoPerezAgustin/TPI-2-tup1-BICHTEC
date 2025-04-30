9b95dcf849dc205167206452dd15c5befdd56c43
SantiagoPerezAgustin <santiagus153@gmail.com>
2025-04-30 18:28:16 -0300

    Resolvi algunso problemas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Carousels from "./pages/Carousels/Carousels.jsx";
 import "./App.css";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
