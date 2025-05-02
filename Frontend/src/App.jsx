8ac43e5c70285ef930b31b1e312126259d43641b
SantiagoPerezAgustin <santiagus153@gmail.com>
2025-05-02 14:21:36 -0300

    Cree el fomulario pero no lo conecte con base de datos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.jsx";
 import Home from "./pages/Home/Home.jsx";
-import Carousels from "./components/CarouselsExample/Carousels.jsx";
 import "./App.css";
 import Footer from "./components/Footer/Footer.jsx";
+import FormPage from "./pages/FormPage/FormPage.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<FormPage />} />
       </Routes>
     </>
   );
---
+++ Frontend/src/pages/FormPage/FormPage.jsx
@@ -0,0 +1,46 @@
+import Login from "../../components/Login/Login";
+import ValidationsForms from "../../components/Validations/ValidationsForms";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function FormPage() {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const repeatPasswordRef = useRef(null);
+    const [errores, setErrores] = useState({});
+    const navigate = useNavigate();
+
+    const manejarEnvio = (FormData) => {
+        const errores = ValidationsForms(FormData);
+
+        if(Object.keys(errores).length > 0) {
+            if (errores.email && emailRef.current) {
+                emailRef.current.focus();
+            } else if (errores.password && passwordRef.current) {
+                passwordRef.current.focus();
+            } else if (errores.repeatPassword && repeatPasswordRef.current) {
+                repeatPasswordRef.current.focus();
+            }
+
+            setErrores(errores);
+        } else {
+            setErrores({});
+            console.log("✅ Formulario válido:", FormData);
+            navigate("/");
+        }
+    };
+
+    return (
+      <>
+        <div>
+          <Login
+            onSubmit={manejarEnvio}
+            errores={errores}
+            refs={{ emailRef, passwordRef, repeatPasswordRef}}
+          />
+        </div>
+      </>
+    );
+}
+
+export default FormPage;
