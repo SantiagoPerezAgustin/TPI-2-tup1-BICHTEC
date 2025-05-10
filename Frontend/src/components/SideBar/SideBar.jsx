1230856dc4606c15894ee08962e6375f3c2c089d
emi-pereyra17 <emiacebal2012@hotmail.com>
2025-05-10 16:24:26 -0300

    Arreglo de Sidebar

--- Frontend/src/components/SideBar/SideBar.jsx
+++ Frontend/src/components/SideBar/SideBar.jsx
@@ -8,33 +8,30 @@ const SideBar = ({ children }) => {
       <div
         className="text-black p-3"
         style={{
-            width: "250px",
-            height: "100%", // Ajusta la altura al contenedor padre
-            boxShadow: "2px 0 5px rgba(0,0,0,0.2)",
-            backgroundColor: "#1a1a1a", // Cambia el color de fondo aquí
-            color: "black", // Cambia el color del texto para que sea legible
-          }}
+          width: "250px",
+          height: "100%", // Ajusta la altura al contenedor padre
+          boxShadow: "2px 0 5px rgba(0,0,0,0.2)",
+          backgroundColor: "#1a1a1a", // Cambia el color de fondo aquí
+          color: "black", // Cambia el color del texto para que sea legible
+        }}
       >
-        <h4 className="text-white">Menú</h4>
+        <br />
         <ul className="nav flex-column">
           <li className="nav-item">
             <a className="nav-link text-white" href="#">
-              Inicio
+              Categorías
             </a>
           </li>
+          <br />
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              Marcas
+            </a>
+          </li>
+          <br />
           <li className="nav-item">
             <a className="nav-link text-white" href="#">
               Servicios
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-white" href="#">
-              Nosotros
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-white" href="#">
-              Contacto
             </a>
           </li>
         </ul>
