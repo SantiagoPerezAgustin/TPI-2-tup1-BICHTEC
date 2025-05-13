0e6dbdc74d5efb36a6b6d6204904ec9bf73a0b3e
SantiagoPerezAgustin <santiagus153@gmail.com>
2025-05-13 10:05:04 -0300

    tuve algunos errores en el commit anterior, ya estan correctos

--- Frontend/src/pages/Productos/Productos.jsx
+++ Frontend/src/pages/Productos/Productos.jsx
@@ -6,7 +6,6 @@ import SideBar from "../../components/SideBar/SideBar";
 const Productos = () => {
   return (
     <>
-      <Header />
       <SideBar>
         <div className="container">
           <h1>Productos</h1>
@@ -14,7 +13,6 @@ const Productos = () => {
           <p>Aquí puedes encontrar una variedad de productos disponibles.</p>
         </div>
       </SideBar>
-      <Footer />
     </>
   );
 };
