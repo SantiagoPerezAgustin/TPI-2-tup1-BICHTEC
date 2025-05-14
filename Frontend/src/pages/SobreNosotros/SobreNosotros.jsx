785015f158928f5ec2fa13816b6c2dcc03dfa92b
Justwaname <26mas26igual50@gmail.com>
2025-05-14 12:45:53 -0300

    correccion de redundancia en CartaSobreNostros, mejora en Footer

--- Frontend/src/pages/SobreNosotros/SobreNosotros.jsx
+++ Frontend/src/pages/SobreNosotros/SobreNosotros.jsx
@@ -1,20 +1,9 @@
 import React from "react";
 import "./SobreNosotros.css";
+import CartaSobreNosotros from "../../components/CartaSobreNosotros/Carta";
 
 const SobreNosotros = () => {
-  return (
-    <div className="sobre-nosotros-container">
-      <div className="sobre-nosotros-card">
-        <h2>Sobre Nosotros</h2>
-        <p>
-          Somos <strong>BICHTEC 📱</strong>, una empresa apasionada por la
-          tecnología móvil. Ofrecemos celulares de las marcas más confiables
-          como iPhone, Samsung y Xiaomi, combinando calidad, asesoramiento y
-          precios competitivos.
-        </p>
-      </div>
-    </div>
-  );
+  return <CartaSobreNosotros></CartaSobreNosotros>;
 };
 
 export default SobreNosotros;
