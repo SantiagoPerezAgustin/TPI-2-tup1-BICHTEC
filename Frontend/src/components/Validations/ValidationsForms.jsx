3adf97e6d2b0aca8cf8e3d37f1365a30783951d2
SantiagoPerezAgustin <santiagus153@gmail.com>
2025-05-22 11:34:31 -0300

    Validaciones en el Frontend

--- Frontend/src/components/Validations/ValidationsForms.jsx
+++ Frontend/src/components/Validations/ValidationsForms.jsx
@@ -1,40 +1,42 @@
 const ValidationsForms = (datos, modo = "register") => {
-    const errores = {};
+  const errores = {};
 
-    if (modo === "register") {
-        if (!datos.nombre?.trim()) {
-            errores.nombre = "El nombre es obligatorio";
-        } else if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/.test(datos.nombre)) {
-            errores.nombre = "El nombre solo debe contener letras y espacios";
-        } else if (datos.nombre.length < 2) {
-            errores.nombre = "El nombre debe tener al menos 2 caracteres";
-        }
+  if (modo === "register") {
+    if (!datos.nombre?.trim()) {
+      errores.nombre = "El nombre es obligatorio";
+    } else if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/.test(datos.nombre)) {
+      errores.nombre = "El nombre solo debe contener letras y espacios";
+    } else if (datos.nombre.length < 2) {
+      errores.nombre = "El nombre debe tener al menos 2 caracteres";
     }
+  }
 
-    if (!datos.email?.trim()){
-        errores.email = "El email es obligatorio";
-    } else if (!/\S+@\S+\.\S+/.test(datos.email)){
-        errores.email = "El email no es válido";
+  if (!datos.email?.trim()) {
+    errores.email = "El email es obligatorio";
+  } else if (!/\S+@\S+\.\S+/.test(datos.email)) {
+    errores.email = "El email no es válido";
+  }
+
+  if (!datos.password?.trim()) {
+    errores.password = "La contraseña es obligatoria";
+  } else if (!/^(?=.*[a-z])(?=.*\d)[a-z\d]{8,}$/i.test(datos.password)) {
+    errores.password = "Mínimo 8 caracteres, incluyendo letras y números";
+  }
+
+  if (modo === "register") {
+    if (!datos.repeatPassword?.trim()) {
+      errores.repeatPassword = "Confirma la contraseña";
+    } else if (
+      !/^(?=.*[a-z])(?=.*\d)[a-z\d]{8,}$/i.test(datos.repeatPassword)
+    ) {
+      errores.repeatPassword =
+        "Mínimo 8 caracteres, incluyendo letras y números";
+    } else if (datos.repeatPassword !== datos.password) {
+      errores.repeatPassword = "La contraseña tiene que ser igual";
     }
+  }
 
-    if (!datos.password?.trim()){
-        errores.password = "La contraseña es obligatoria";
-    } else if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(datos.password)) {
-        errores.password = "Mínimo 8 caracteres, incluyendo letras y números";
-    }
-
-    if (modo === "register") {
-        if(!datos.repeatPassword?.trim()) {
-            errores.repeatPassword = "Confirma la contaseña";
-        } else if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(datos.repeatPassword)) {
-            errores.repeatPassword =
-              "Mínimo 8 caracteres, incluyendo letras y números";
-        } else if(datos.repeatPassword !== datos.password) {
-            errores.repeatPassword = "La contraseña tiene que ser igual";
-        }
-    }
-
-    return errores;
-}
+  return errores;
+};
 
 export default ValidationsForms;
